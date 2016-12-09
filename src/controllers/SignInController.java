9bb78bc1e66a3efb109e60dfd1886b8b8ce117a9
“anton” <antong@mintigo.com>
2016-12-09 16:52:59 +0200

    basic ui and login page

---
+++ src/controllers/SignInController.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import module.UserBean;
+
+/**
+ * Servlet implementation class SignInController
+ */
+@WebServlet("/SignInController")
+public class SignInController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignInController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		UserBean user = new UserBean();
+		
+		user.setFirstName(request.getParameter("first_name"));
+		user.setLastName(request.getParameter("last_name"));
+	    user.setUserName(request.getParameter("email"));
+	    user.setPassword(request.getParameter("password"));
+		doGet(request, response);
+	}
+
+}
